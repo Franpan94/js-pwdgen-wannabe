76d435f3732cfb8a5b9c7bfe1cb88ab465423ed1
Franpan94 <francescopanico94@libero.it>
2022-06-13 17:03:22 +0200

    password generated

--- js/script.js
+++ js/script.js
@@ -2,8 +2,10 @@
 const username= prompt('Inserisci nome');
 const usersurname=prompt('Inserisci cognome');
 const usercolor=prompt('Inserisci il tuo colore preferito');
+const userpassword= username + usersurname + usercolor +'22';
 
 // Associazione variabili a id html
 document.getElementById('user-name').innerHTML= username;
 document.getElementById('user-surname').innerHTML= usersurname;
 document.getElementById('user-color').innerHTML= usercolor;
+document.getElementById('user-password').innerHTML= userpassword;
